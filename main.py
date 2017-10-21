dd574e20e6c81d9b0bf44b8622faeb097b417671
jaimevalero78 <jaimevalero78@yahoo.es>
2017-10-21 14:28:35 +0200

    Before deleting hd accesses

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from pathlib import Path
 import sys
 import requests
 import json
@@ -7,6 +8,9 @@ from   requests.auth import HTTPBasicAuth
 import pyjq
 from scipy.spatial import distance
 from scipy.spatial.distance import squareform, pdist
+import pickle
+import time
+
 
 TAG_MATRIX = pd.DataFrame()
 
@@ -22,10 +26,11 @@ def Load_Starred_Repos() :
     df['Tags'] = df['Tags'] + ","
     df.to_csv("clean_TopStaredRepositories.csv")
     df        = pd.read_csv('clean_TopStaredRepositories.csv')
-    df_backup = pd.read_csv('clean_TopStaredRepositories.csv')
+
+    #df_backup = pd.read_csv('clean_TopStaredRepositories.csv')
     #
     df['Language']         = df.loc[:,'Language'].str.lower()
-    df_backup['Language']  = df_backup.loc[:,'Language'].str.lower()
+    df_backup = df.copy(deep=True)
     return df, df_backup
 
 def Load_User_Repos(github_user):
@@ -38,9 +43,13 @@ def Load_User_Repos(github_user):
     return r.json()
 
 def Generate_Tag_Matrix(df):
+  my_file = Path("Generate_Tag_Matrix.data")
 
-  if ( TAG_MATRIX > 0) :
-      return TAG_MATRIX
+  if my_file.exists():
+    #print ("Cargamos el Tag maxtrix")
+    with open(r"Generate_Tag_Matrix.data", "rb") as input_file:
+      df = pickle.load(input_file)
+    return df
   else :
 
     # Generate tag list
@@ -66,7 +75,8 @@ def Generate_Tag_Matrix(df):
     for column in COLUMNS_TO_REMOVE_LIST: del df[column]
     df.columns = df.columns.str.lower()
 
-    TAG_MATRIX = df.copy(deep=True)
+    with open(r"Generate_Tag_Matrix.data", "wb") as output_file:
+        pickle.dump(df, output_file)
 
   return df
 
@@ -101,18 +111,18 @@ def Enrich_Tag_Matrix(df,json_response,i):
     repo_description = pyjq.all(".[%s] | .description" %i,  json_response )
     repo_topics      = pyjq.all(".[%s] | .topics"      %i,  json_response )
     #
-    print( repo_description, repo_names)
+    #print( repo_description, repo_names)
     if repo_description[0] is None : repo_description=['kk']
 
     new_element = pd.DataFrame( 0,[df.index.max()+1],columns=df.columns)
     for j in ( repo_names[0].split('-') + repo_languages + repo_description[0].replace(".", " ").replace(",", " ").split() + list(repo_topics[0]) ) :
         if j is not None:
             if j.lower()  in df.columns :
-                print("Setting to 1", j.lower())
+                #print("Setting to 1", j.lower())
                 new_element[j.lower()] = 1
     # Concat new user repo dataframe to stared repos dataframe
     df = pd.concat([df,new_element])
-    df.to_csv("Enrich_Tag_Matrix.csv")
+    #df.to_csv("Enrich_Tag_Matrix.csv")
 
     return df,repo_names
 
@@ -123,56 +133,76 @@ def Generate_Distance_Matrix(df_backup,df,repo_names):
     res = pdist(df, 'euclidean')
     squareform(res)
     df_dist = pd.DataFrame(squareform(res), index=repos, columns= repos)
-    df_dist.to_csv("Distance_Matrix.csv")
+    #df_dist.to_csv("Distance_Matrix.csv")
     return df_dist
 
 def Get_Closest_Repo(repo):
+  result_array = []
   i = df_dist.columns[-1]
   df_dist.loc[df_dist[i] == 0, i] = 1000
   min = df_dist[i].min()
-  #similar =df_dist.loc[df_dist[i] == min].index[0]
   kk = df_dist[i]
   print ( kk[df_dist[i] == min].index, i, min)
   for recomended_repo in (kk[df_dist[i] == min].index):
     description = df_backup.loc[df_backup['Url'] == ('http://github.com/%s' %recomended_repo ), 'Description'].iloc[0]
-    print([min,i,recomended_repo,description])
+    #print([min,i,recomended_repo,description])
+    result_array.append( {
+      "score" : min,
+      "user_repo_name"  : i ,
+      "recomended_repo_name" : recomended_repo ,
+      "recomended_repo_description" : description })
+    #print (result_array[-1])
 
-  os.chdir("/Users/jaimevalerodebernabe/git/github-recommendation-engine")
+  return result_array
 
 #MAIN
+start = time.time()
+# run your code
+
+os.chdir("/Users/jaimevalerodebernabe/git/github-recommendation-engine")
 
 # Load Data and user Repos
 json_response = Load_User_Repos(github_user="jaimevalero")
 
+print ((start - time.time(),"Datos Cargados"))
+
 # For each repo
-i=1
 num_repos = len(pyjq.all(".[] | .name"  ,  json_response ),)
 print(num_repos)
-for i in range(15):
+for i in range(num_repos):
+
     df, df_backup = Load_Starred_Repos()
 
+    print ((start - time.time(),"Load_Starred_Repos done"))
+
     df = Generate_Tag_Matrix(df)
+    print ((start - time.time(),"Generate_Tag_Matrix done"))
+
 
     #add repo to tag_matrix
     df,repo_names = Enrich_Tag_Matrix(df,json_response,i)
+    print ((start - time.time(),"Enrich_Tag_Matrix done"))
 
     #calculate_distance_matrix
     df_dist = Generate_Distance_Matrix(df_backup,df,repo_names)
+    print ((start - time.time(),"Generate_Distance_Matrix done"))
 
     #print nearest result
     Get_Closest_Repo(df_dist)
+    print ((start - time.time(),"Get_Closest_Repo done"))
 
  # All
 
-print("EEEEEEEXXXTTRRAA")
+def Extra_Salida() :
+    print("EEEEEEEXXXTTRRAA")
 
-seguir = False
-if seguir :
-    df, df_backup = Load_Starred_Repos()
-    df = Generate_Tag_Matrix(df)
-    #add repo to tag_matrix
-    df,repo_names = Enrich_All_Tag_Matrix(df,json_response,i)
-    #calculate_distance_matrix
-    df_dist = Generate_Distance_Matrix(df_backup,df,repo_names)
-    #print nearest result
-    Get_Closest_Repo(df_dist)
+    seguir = False
+    if seguir :
+        df, df_backup = Load_Starred_Repos()
+        df = Generate_Tag_Matrix(df)
+        #add repo to tag_matrix
+        df,repo_names = Enrich_All_Tag_Matrix(df,json_response,i)
+        #calculate_distance_matrix
+        df_dist = Generate_Distance_Matrix(df_backup,df,repo_names)
+        #print nearest result
+        Get_Closest_Repo(df_dist)
